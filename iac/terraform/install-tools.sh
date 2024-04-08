25fafb2b2a507e57fa01996f9ec7912f0b0d0c10
alfinv <alfi.nv@gmail.com>
2024-04-08 17:51:55 +0000

    script

--- iac/terraform/install-tools.sh
+++ iac/terraform/install-tools.sh
@@ -52,6 +52,7 @@ sudo apt install jq -y
 
 # Install Terraform
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt install terraform -y
